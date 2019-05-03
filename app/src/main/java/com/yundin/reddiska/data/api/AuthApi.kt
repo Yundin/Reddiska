ad5a164607eed80e8fb50611e32d6ef76a57acf5
Vladislav Yundin <4yundin@gmail.com>
2019-05-03 15:19:40 +0300

    Added auth header to api

--- app/src/main/java/com/yundin/reddiska/data/api/AuthApi.kt
+++ app/src/main/java/com/yundin/reddiska/data/api/AuthApi.kt
@@ -1,12 +1,12 @@
 package com.yundin.reddiska.data.api
 
 import io.reactivex.Single
-import retrofit2.http.Field
-import retrofit2.http.POST
-import retrofit2.http.Url
+import retrofit2.http.*
 
 interface AuthApi {
 
+    @Headers("Authorization: Basic bEQ3MTJ1TC00b3c0cnc6")
+    @FormUrlEncoded
     @POST
     fun authorizeApp(@Url url: String, @Field("device_id") deviceId: String, @Field("grant_type") grantType: String = "https://oauth.reddit.com/grants/installed_client"): Single<AppAuthResponse>
 }
