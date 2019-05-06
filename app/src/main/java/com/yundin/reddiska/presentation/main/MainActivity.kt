8fd51af3bf8ac99e4838529b5703b85ed2b3f78f
Vladislav Yundin <4yundin@gmail.com>
2019-05-06 18:12:49 +0300

    Injecting everything in MainActivity

--- app/src/main/java/com/yundin/reddiska/presentation/main/MainActivity.kt
+++ app/src/main/java/com/yundin/reddiska/presentation/main/MainActivity.kt
@@ -3,20 +3,26 @@ package com.yundin.reddiska.presentation.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.yundin.reddiska.R
+import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
         viewModel.posts.observe(this, Observer {
             text.text = when {
                 it.isSuccess() -> it.data!!.joinToString()
