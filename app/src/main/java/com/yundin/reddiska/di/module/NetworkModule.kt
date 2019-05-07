8e44ae81f3809d0f4c401429fff7ff57c7071e4b
Vladislav Yundin <4yundin@gmail.com>
2019-05-07 20:46:00 +0300

    Logging network calls

--- app/src/main/java/com/yundin/reddiska/di/module/NetworkModule.kt
+++ app/src/main/java/com/yundin/reddiska/di/module/NetworkModule.kt
@@ -1,9 +1,12 @@
 package com.yundin.reddiska.di.module
 
 import com.yundin.reddiska.data.api.AuthApi
+import com.yundin.reddiska.data.api.PostsApi
 import com.yundin.reddiska.util.LiveDataCallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -11,11 +14,22 @@ import javax.inject.Singleton
 @Module
 class NetworkModule {
 
+    @Provides
+    fun provideOkhttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://oauth.reddit.com")
+            .client(client)
             .addCallAdapterFactory(LiveDataCallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -23,4 +37,7 @@ class NetworkModule {
 
     @Provides
     fun provideAuthApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
+
+    @Provides
+    fun providePostsApi(retrofit: Retrofit): PostsApi = retrofit.create(PostsApi::class.java)
 }
