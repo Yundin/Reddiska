7f3a5f0688f169298ef2d5a548b2d4f87c140679
Vladislav Yundin <4yundin@gmail.com>
2019-05-09 15:38:54 +0300

    Binding posts directly to xml

--- app/src/main/java/com/yundin/reddiska/presentation/main/MainActivity.kt
+++ app/src/main/java/com/yundin/reddiska/presentation/main/MainActivity.kt
@@ -2,33 +2,25 @@ package com.yundin.reddiska.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.yundin.reddiska.R
+import com.yundin.reddiska.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
-
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.posts.observe(this, Observer {
-            text.text = when {
-                it.isSuccess() -> it.toString()
-                it.isError() -> it.errorMessage!!
-                else -> "LOADING"
-            }
-        })
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewmodel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
     }
 }
